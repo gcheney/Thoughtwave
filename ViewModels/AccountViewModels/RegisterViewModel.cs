d069d14b7523e90bc50f6c8320dd272f2b552304
Glendon Cheney <glendon.cheney@gmail.com>
2016-11-30 08:37:18 -0600

    Update account and manage viewmodel validation attributes

--- ViewModels/AccountViewModels/RegisterViewModel.cs
+++ ViewModels/AccountViewModels/RegisterViewModel.cs
@@ -14,12 +14,16 @@ namespace Sophophile.ViewModels.AccountViewModels
         public string Email { get; set; }
 
         [Required]
-        [StringLength(25, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [StringLength(25, 
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
+            MinimumLength = 3)]
         [Display(Name = "Username")]
         public string Username { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(100, 
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
+            MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
