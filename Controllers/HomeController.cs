9d80427922fabe66da127e6c35826ba9ba38e849
Glendon Cheney <glendon.cheney@gmail.com>
2016-12-04 11:25:35 -0600

    Add recent articles to home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,12 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Thoughtwave.Data;
+using Thoughtwave.Models;
 
 namespace Thoughtwave.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private IThoughtwaveRepository _repository;
+        private ILogger<HomeController> _logger;
+
+        public HomeController(IThoughtwaveRepository repository, 
+            ILogger<HomeController> logger)
         {
-            return View();
+            _repository = repository;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var articles = await _repository.GetRecentArticlesAsync();
+            
+            return View(articles);
         }
 
         public IActionResult Error()
