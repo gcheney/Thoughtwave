22f9cfcfbc1276a44eccd0417b3e315177116b1c
Glendon Cheney <glendon.cheney@gmail.com>
2016-12-18 02:44:47 -0600

    Add edit and create view models

---
+++ ViewModels/ThoughtViewModels/CreateThoughtViewModel.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Thoughtwave.Models;
+
+namespace Thoughtwave.ViewModels.ThoughtViewModels
+{
+    public class CreateThoughtViewModel
+    {
+        [Required(ErrorMessage = "Please include a title for your thought")]
+        [StringLengthAttribute(50, 
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
+            MinimumLength = 3)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please include your thought content")]
+        [Display(Name = "Your Thought")]
+        public string Content { get; set; }
+
+        [Required(ErrorMessage = "Please choose a category for your thought")]
+        [Display(Name = "Category (defaults to personal)")]
+        public Category Category { get; set; }
+
+        [Display(Name = "Disable Comments?")]
+        public bool DisableComments { get; set; }
+    }
+}
--- ViewModels/ThoughtViewModels/EditThoughtViewModel.cs
+++ ViewModels/ThoughtViewModels/EditThoughtViewModel.cs
@@ -1,25 +1,28 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using Thoughtwave.Models;
 
 namespace Thoughtwave.ViewModels.ThoughtViewModels
 {
-    public class ThoughtViewModel
+    public class EditThoughtViewModel
     {
-        [Required(ErrorMessage = "Please include a title for your thought")]
+        [Required(ErrorMessage = "Please keep a title for your thought")]
         [StringLengthAttribute(50, 
             ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
             MinimumLength = 3)]
         public string Title { get; set; }
 
-        [Required(ErrorMessage = "Please include your thought content")]
+        [Required(ErrorMessage = "Please keep your thought content")]
         [Display(Name = "Your Thought")]
         public string Content { get; set; }
 
-        [Required(ErrorMessage = "Please choose a category for your thought")]
+        [Required(ErrorMessage = "Please select a category for your thought")]
         [Display(Name = "Category (defaults to personal)")]
         public Category Category { get; set; }
 
         [Display(Name = "Disable Comments?")]
         public bool DisableComments { get; set; }
+
+        public DateTime CreatedOn { get; set; }
     }
 }
