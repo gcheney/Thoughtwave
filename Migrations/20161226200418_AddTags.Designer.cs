f2b359bbb66285319af0fc1dadf35ada0c55e9b1
Glendon Cheney <glendon.cheney@gmail.com>
2016-12-26 16:34:04 -0600

    Add migrations for thought model update

--- Migrations/20161226200418_AddTags.Designer.cs
+++ Migrations/20161226200418_AddTags.Designer.cs
@@ -8,9 +8,10 @@ using Thoughtwave.Data;
 namespace Thoughtwave.Migrations
 {
     [DbContext(typeof(ThoughtwaveDbContext))]
-    partial class ThoughtwaveDbContextModelSnapshot : ModelSnapshot
+    [Migration("20161226200418_AddTags")]
+    partial class AddTags
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.0.1");
---
+++ Migrations/20161226200418_AddTags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Thoughtwave.Migrations
+{
+    public partial class AddTags : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Thoughts",
+                nullable: true);
+        }
+    }
+}
