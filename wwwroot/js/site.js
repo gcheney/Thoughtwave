ffb444a004b2124e0dab616db82abecaf2ff197d
Glendon Cheney <glendon.cheney@gmail.com>
2016-12-30 17:15:53 -0600

    Refactor jquery code

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,79 +1,94 @@
-$(document).ready(function() {
+/*
+ * Custom scripts for Thoughtwave
+ */
 
-    // Navigation script to Show Header on Scroll-Up
-    //primary navigation slide-in effect
-    if ($(window).width() > 1170) {
-        var headerHeight = $('.navbar-custom').height();
-        $(window).on('scroll', {
-                previousTop: 0
-            }, function()   {
-                var currentTop = $(window).scrollTop();
-                //check if user is scrolling up
-                if (currentTop < this.previousTop) {
-                    //if scrolling up...
-                    if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-visible');
-                    } else {
-                        $('.navbar-custom').removeClass('is-visible is-fixed');
+(function($, W, D) { 
+    var JQUERY = {};
+
+    JQUERY.UTIL = {
+        initNavigationSlide: function() {
+            if ($(W).width() > 1170) {
+                var headerHeight = $('.navbar-custom').height();
+                $(W).on('scroll', {
+                        previousTop: 0
+                    }, function()   {
+                        var currentTop = $(W).scrollTop();
+                        //check if user is scrolling up
+                        if (currentTop < this.previousTop) {
+                            //if scrolling up...
+                            if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+                                $('.navbar-custom').addClass('is-visible');
+                            } else {
+                                $('.navbar-custom').removeClass('is-visible is-fixed');
+                            }
+                        } else {
+                            //if scrolling down...
+                            $('.navbar-custom').removeClass('is-visible');
+                            if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
+                                $('.navbar-custom').addClass('is-fixed');
+                            }
+                        }
+                        this.previousTop = currentTop;
                     }
-                } else {
-                    //if scrolling down...
-                    $('.navbar-custom').removeClass('is-visible');
-                    if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) {
-                        $('.navbar-custom').addClass('is-fixed');
-                    }
-                }
-                this.previousTop = currentTop;
+                );
             }
-        );
-    }
-
-    // navbar search form 
-    $('a[href="#search"]').on('click', function(e) {
-        e.preventDefault();
-        $('#search').addClass('open');
-        $('#search > form > input[type="search"]').focus();
-    });
-    
-    $('#search, #search button.close').on('click keyup', function(e) {
-        if (e.target == this || e.target.className == 'close' || e.keyCode == 27) {
-            $(this).removeClass('open');
-        }
-    });
-
-    // search panel form
-    $('.search-panel .dropdown-menu').find('a').click(function(e) {
-		e.preventDefault();
-		var param = $(this).attr("href").replace("#","");
-		var category = $(this).text();
-		$('.search-panel span#search_category').text(category);
-		$('.input-group #category').val(param);
-	});
-
-
-    // Handle logout form link
-    $('#logoutLink').on('click', function(e) {
-        e.preventDefault();
-        $('#logoutForm').submit();
-    });
-
-    // gracefully fadeout flash message on close
-    function flashFadeout() {
-        $('#flash-message').fadeTo('fast', 0.00, function() { 
-                $(this).slideUp('slow', function() { 
-                    $(this).remove(); 
+        },
+        initNavbarSerach: function() {
+            $('a[href="#search"]').on('click', function(e) {
+                e.preventDefault();
+                $('#search').addClass('open');
+                $('#search > form > input[type="search"]').focus();
+            });
+            
+            $('#search, #search button.close').on('click keyup', function(e) {
+                if (e.target == this || e.target.className == 'close' || e.keyCode == 27) {
+                    $(this).removeClass('open');
+                }
+            });
+        },
+        initSearchPanelForm: function() {
+            $('.search-panel .dropdown-menu').find('a').click(function(e) {
+                e.preventDefault();
+                var param = $(this).attr("href").replace("#","");
+                var category = $(this).text();
+                $('.search-panel span#search_category').text(category);
+                $('.input-group #category').val(param);
+            });
+        },
+        hanldeLogoutForm: function() {
+            $('#logoutLink').on('click', function(e) {
+                e.preventDefault();
+                $('#logoutForm').submit();
+            });
+        },
+        fadeOutFlashMessages: function() {
+            function flashFadeout() {
+                $('#flash-message').fadeTo('fast', 0.00, function() { 
+                        $(this).slideUp('slow', function() { 
+                            $(this).remove(); 
+                        });
                 });
-        });
+            }
+
+            // close flash message
+            $('.close').on('click', function(e) {
+                e.preventDefault();
+                flashFadeout();
+            });
+
+            setTimeout(function() {
+                flashFadeout();
+            }, 10000);
+        }
     }
 
-    setTimeout(function() {
-        flashFadeout();
-    }, 10000);
-
-    // close flash message
-    $('.close').on('click', function(e) {
-        e.preventDefault();
-        flashFadeout();
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY.UTIL.initNavigationSlide();
+        JQUERY.UTIL.initNavbarSerach();
+        JQUERY.UTIL.initSearchPanelForm();
+        JQUERY.UTIL.hanldeLogoutForm();
+        JQUERY.UTIL.fadeOutFlashMessages();
     });
-    
-});
+
+})(jQuery, window, document);
