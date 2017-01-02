65dbfd6001620a88758d2b7c1882155988595b48
Glendon Cheney <glendon.cheney@gmail.com>
2017-01-02 02:08:01 -0600

    Add username to external login confirmation

--- ViewModels/AccountViewModels/ExternalLoginConfirmationViewModel.cs
+++ ViewModels/AccountViewModels/ExternalLoginConfirmationViewModel.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Thoughtwave.ViewModels.AccountViewModels
 {
@@ -11,5 +7,12 @@ namespace Thoughtwave.ViewModels.AccountViewModels
         [Required]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(25, 
+            ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", 
+            MinimumLength = 3)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
     }
 }
