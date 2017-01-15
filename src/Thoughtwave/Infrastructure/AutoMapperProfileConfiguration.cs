56ed4000d51926686175e407db4520258e0c6ba1
Glendon Cheney <glendon.cheney@gmail.com>
2017-01-15 10:40:41 -0600

    Set up Automapper profile

---
+++ src/Thoughtwave/Infrastructure/AutoMapperProfileConfiguration.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using System.Net;
+using Thoughtwave.Models;
+using Thoughtwave.ExtensionMethods;
+using Thoughtwave.ViewModels.ThoughtViewModels;
+using Thoughtwave.ViewModels.ManageViewModels;
+
+namespace Thoughtwave.Infrastructure
+{
+    public class AutoMapperProfileConfiguration : Profile
+    {
+        protected override void Configure()
+        {
+            CreateMap<Thought, ThoughtViewModel>();
+
+            CreateMap<CreateThoughtViewModel, Thought>()
+                .BeforeMap((src, dest) => src.Content = WebUtility.HtmlEncode(src.Content))
+                .BeforeMap((src, dest) => src.Tags = src.Tags.RemoveWhiteSpaces());
+
+            CreateMap<EditThoughtViewModel, Thought>()
+                .BeforeMap((src, dest) => src.Content = WebUtility.HtmlEncode(src.Content))
+                .BeforeMap((src, dest) => src.Tags = src.Tags.RemoveWhiteSpaces())
+                .ReverseMap()
+                .AfterMap((src, dest) => dest.Content = WebUtility.HtmlDecode(dest.Content));
+
+            CreateMap<EditProfileViewModel, User>().ReverseMap();
+        }
+    }
+}
